423c6a1aca2889b3bf87e12c881c70aadc60b0f8
王天赐 <15565946702@163.com>
2022-11-22 16:06:50 +0800

    王天赐, feat: el表达式的使用

---
+++ jsp-codes/src/main/java/cn/knightzz/servlet/HelloServlet.java
@@ -0,0 +1,39 @@
+package cn.knightzz.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 王天赐
+ * @title: HelloServlet
+ * @projectName zjq-codes
+ * @description:
+ * @website <a href="http://knightzz.cn/">http://knightzz.cn/</a>
+ * @github <a href="https://github.com/knightzz1998">https://github.com/knightzz1998</a>
+ * @create: 2022-11-22 14:31
+ */
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Integer id = (Integer) req.getSession().getAttribute("id");
+        System.out.println("=========================" + id);
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+}
---
+++ jsp-codes/src/main/webapp/hello.jsp
@@ -0,0 +1,43 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: knight'z'z
+  Date: 2022/11/22
+  Time: 14:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page isELIgnored="false" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<%@ page import="java.io.*,java.util.*" %>
+<html>
+<head>
+	<title>Title</title>
+</head>
+<body>
+
+<%
+	String id = request.getParameter("id");
+	out.print(id);
+	session.setAttribute("id", Integer.valueOf(id));
+%>
+
+<%--
+	<%@ page isELIgnored="false" %> 必须加这个啊, 不然解析不了
+--%>
+<h2>2 ${sessionScope.id}</h2>
+
+<a href="javascript:void(0)" onclick="toHelloServlet(${requestScope.id})"> 跳转到 HelloServlet </a>
+<a href="hello-servlet"> 跳转到 HelloServlet2 </a>
+
+<h1>hello</h1>
+
+<script>
+
+    function toHelloServlet(id) {
+        console.log("................" + id)
+    }
+</script>
+
+</body>
+</html>
